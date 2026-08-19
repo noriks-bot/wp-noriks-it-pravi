30630e965c1e0457ad1b7ad3f282db65beb9a1c1
Noriks (via Claude) <info@noriks.com>
2026-08-19 12:43:42 +0200

    Traka sezonske rasprodaje na kategoriji (prevedeno) + HTML tablica bokserica manja na mobitelu

--- wp-content/themes/noriks/functions.php
+++ wp-content/themes/noriks/functions.php
@@ -7,6 +7,7 @@
 
 include(get_template_directory() . '/functions/product-type.php'); // central product-type resolver (load first)
 include(get_template_directory() . '/functions/pack-switcher.php'); // scelta della dimensione della confezione + altre combinazioni di colori
+include(get_template_directory() . '/functions/flash-deals-banner.php'); // traka sezonske rasprodaje
 include(get_template_directory() . '/functions/checkout_mods.php');
 include(get_template_directory() . '/functions/thankyou_upsell.php');
 include(get_template_directory() . '/functions/product-page-upsell.php'); // upsell okvir ispod gumba na stranici proizvoda (ACF prekidac)
