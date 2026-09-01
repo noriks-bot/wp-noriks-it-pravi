577584968670f5b06ad8854d4f19d382196fd5e3
Noriks (via Claude) <info@noriks.com>
2026-09-01 09:58:18 +0200

    Umik obljube o brezplacni zamenjavi tudi v pogostih vprasanjih; brezplacna dostava ostane nedotaknjena

--- wp-content/themes/noriks/template_parts/single-product-bottom-nicer.php
+++ wp-content/themes/noriks/template_parts/single-product-bottom-nicer.php
@@ -1919,7 +1919,7 @@ $faq_pick = function( $title, $list ) use ( $is_ortopas_faq, $ortopas_faq, $is_b
               ?>
                     <div class="faq-item">
                       <button class="faq-question">
-                         <?php echo $faq_item["questioon"]; ?>
+                         <?php echo function_exists('noriks_no_free_exchange') ? noriks_no_free_exchange( $faq_item["questioon"] ) : $faq_item["questioon"]; ?>
                         <span class="arrow">&#9660;</span>
                       </button>
                       <div class="faq-answer">
@@ -1946,7 +1946,7 @@ $faq_pick = function( $title, $list ) use ( $is_ortopas_faq, $ortopas_faq, $is_b
               ?>
                     <div class="faq-item">
                       <button class="faq-question">
-                         <?php echo $faq_item["questioon"]; ?>
+                         <?php echo function_exists('noriks_no_free_exchange') ? noriks_no_free_exchange( $faq_item["questioon"] ) : $faq_item["questioon"]; ?>
                         <span class="arrow">&#9660;</span>
                       </button>
                       <div class="faq-answer">
@@ -1973,7 +1973,7 @@ $faq_pick = function( $title, $list ) use ( $is_ortopas_faq, $ortopas_faq, $is_b
               ?>
                     <div class="faq-item">
                       <button class="faq-question">
-                         <?php echo $faq_item["questioon"]; ?>
+                         <?php echo function_exists('noriks_no_free_exchange') ? noriks_no_free_exchange( $faq_item["questioon"] ) : $faq_item["questioon"]; ?>
                         <span class="arrow">&#9660;</span>
                       </button>
                       <div class="faq-answer">
